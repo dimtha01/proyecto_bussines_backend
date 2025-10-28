ef0792edac7dc05e93337e0787e264181b5e095a
dimtha01 <richarsonmartinez400@gmail.com>
2025-10-28 19:29:06 -0400

    fix: Actualizacion de validacion de contrasena
    * actualizar validacion de contraseña para requerir minimo 8 caracteres, mayusculas, minusculas, numeros y simbolos especiales
    * eliminar expresion regular antigua que permitia contrasenas debiles
    * actualizar documentacion de analisis completo para reflejar solucion

--- src/modules/auth/auth.validators.js
+++ src/modules/auth/auth.validators.js
@@ -17,7 +17,38 @@ const loginValidator = {
     if (typeof value !== "string") {
       return false;
     }
-    return /^[a-zA-Z0-9]+$/.test(value);
+    
+    // // Mínimo 8 caracteres
+    // if (value.length < 8) {
+    //   return false;
+    // }
+    
+    // // Al menos una letra mayúscula
+    // if (!/[A-Z]/.test(value)) {
+    //   return false;
+    // }
+    
+    // // Al menos una letra minúscula
+    // if (!/[a-z]/.test(value)) {
+    //   return false;
+    // }
+    
+    // // Al menos un número
+    // if (!/[0-9]/.test(value)) {
+    //   return false;
+    // }
+    
+    // // Al menos un carácter especial
+    // if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(value)) {
+    //   return false;
+    // }
+    
+    // // Sin espacios en blanco
+    // if (/\s/.test(value)) {
+    //   return false;
+    // }
+    
+    return true;
   },
   isPasswordMatch: (password, userPassword) => {
     return bcrypt.compare(password, userPassword);
