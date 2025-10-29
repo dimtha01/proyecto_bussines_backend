e43348f54d16894e8c708cb1fb049457891f69e9
dimtha01 <richarsonmartinez400@gmail.com>
2025-10-29 13:29:52 -0400

    refactor: eliminar controlador y ruta estatus
    * eliminar el archivo estatus.controller.js que gestionaba la lógica de estatus
    * eliminar el archivo estatus.routes.js que exponía la ruta /estatus
    * actualizar referencias internas y documentación para reflejar la eliminación
    * eliminar importaciones y dependencias vinculadas a los archivos eliminados

--- src/routes/estatus.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getEstatus } from "../controllers/estatus.controller.js";
-
-const router = Router();
-
-router.get("/estatus", getEstatus);
-
-export default router;
