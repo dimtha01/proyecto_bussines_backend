0f5a47e56bc8dcf4192af3964d946bf1a076ef66
dimtha01 <richarsonmartinez400@gmail.com>
2025-10-29 16:56:10 -0400

    chore: eliminar rutas y controladores de clientes
    * eliminar rutas y controladores de clientes
    * eliminar importación y uso de employeesRoutes en app.js
    * eliminar archivos clients.routes.js y clients.controller.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors"; // Importa el paquete cors
-import employeesRoutes from "./routes/clients.routes.js";
 import proyectsRoutes from "./routes/proyects.routes.js";
 import avanceFisicoRoutes from "./routes/avance_fisico.routes.js";
 import avanceFinancieroRoutes from "./routes/avance_financiero.routes.js";
@@ -40,7 +39,6 @@ app.use(express.json());
   
 // Routes
 app.use("/", indexRoutes);
-app.use("/api", employeesRoutes);
 app.use("/api", proyectsRoutes);
 app.use("/api", avanceFisicoRoutes);
 app.use("/api", avanceFinancieroRoutes);
