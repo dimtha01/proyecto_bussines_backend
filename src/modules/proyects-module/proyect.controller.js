39237b648e1721080bcfe57ada9b461339aaf434
dimtha01 <richarsonmartinez400@gmail.com>
2025-11-01 14:13:41 -0400

    fix: corregir manejo de errores en controlador
    * corregir manejo de errores en controlador de proyectos
    * encapsular lógica en bloque try/catch
    * retornar código 500 y mensaje genérico al fallar

--- src/modules/proyects-module/proyect.controller.js
+++ src/modules/proyects-module/proyect.controller.js
@@ -1,8 +1,14 @@
 import { proyectsModel } from "./proyect.model.js";
 
 export const getProyects = async (req, res) => {
+  try {
+    const result = await proyectsModel.getProyects();
+    res.status(result.status).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Algo salió mal" });
+  }
 
-  const result = await proyectsModel.getProyects();
-  res.status(result.status).json(result);
+
 
 }
